ca074d5975886e04e7ebd00a730c5fd5e4ff3aba
IsaSam <isaacsamuel27@gmail.com>
2018-11-12 15:32:08 -0500

    popularView created

---
+++ FlixApp/ViewControllers/PopularMoviesViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PopularMoviesViewController.swift
+//  MovieModel
+//
+//  Created by Isaac Samuel on 11/12/18.
+//  Copyright © 2018 Isaac Samuel. All rights reserved.
+//
+
+import UIKit
+
+class PopularMoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
